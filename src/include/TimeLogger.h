f6f55c6544c52c88359cb4dcce7d140a098e7b39
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2016-06-09 16:51:36 -0500

    Changes to TimeLogger.h

--- src/include/TimeLogger.h
+++ src/include/TimeLogger.h
@@ -9,6 +9,28 @@
 #ifndef Camellia_TimeLogger_h
 #define Camellia_TimeLogger_h
 
+#include "Teuchos_RCP.hpp"
 
+class Epetra_Time;
+
+namespace Camellia {
+  class TimeLogger
+  {
+    static Teuchos::RCP<TimeLogger> _sharedInstance;
+  
+    std::vector<std::string> _timerNames;
+    std::vector<Teuchos::RCP<Epetra_Time>> _timers;
+    std::vector<int> _inactiveTimerHandles;
+    std::map<std::string,double> _totalTimes;
+  public:
+    int startTimer(const std::string &timerName);
+    void stopTimer(int timerHandle);
+    
+    double totalTime(const std::string &timerName) const;
+    const std::map<std::string,double> totalTimes() const;
+    
+    static Teuchos::RCP<TimeLogger> sharedInstance();
+  };
+}
 
 #endif
