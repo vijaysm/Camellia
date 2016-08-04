69bf8cd96c995be87b4fcd391a4641d2959d0edd
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2016-08-04 17:06:35 -0500

    Added Camellia.h.

---
+++ src/include/Camellia.h
@@ -0,0 +1,25 @@
+// @HEADER
+//
+// © 2016 UChicago Argonne.  For licensing details, see LICENSE-Camellia in the licenses directory.
+//
+//@HEADER
+
+// Header file suitable for #including in drivers that use Camellia.
+
+#ifndef Camellia_Camellia_h
+#define Camellia_Camellia_h
+
+#include "BC.h"
+#include "BF.h"
+#include "Function.h"
+#include "LinearTerm.h"
+#include "Mesh.h"
+#include "MeshFactory.h"
+#include "RHS.h"
+#include "Solution.h"
+#include "Solver.h"
+#include "TypeDefs.h"
+#include "Var.h"
+#include "VarFactory.h"
+
+#endif
